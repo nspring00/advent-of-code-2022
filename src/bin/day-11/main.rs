58a9f2fb6cbe633b6b3bd6dff8d4f826e0df1c1b
Nikolaus Spring <nikolaus.spring@gmail.com>
2023-08-18 15:14:28 +0200

    Solve AoC 22.11 part 1

--- src/bin/day-11/main.rs
+++ src/bin/day-11/main.rs
@@ -1,16 +1,20 @@
-#[derive(Debug, Clone)]
+use std::fmt::Debug;
+use std::fs;
+use std::str::FromStr;
+use regex::Regex;
+
 struct Monkey {
     id: usize,
-    items: Vec<u32>,
-    operation: fn(u32) -> u32,
-    divider: u32,
+    items: Vec<u64>,
+    operation: Box<dyn Fn(u64) -> u64>,
+    divider: u64,
     true_monkey: usize,
     false_monkey: usize,
     inspect_count: u32,
 }
 
 impl Monkey {
-    fn new(id: usize, items: Vec<u32>, operation: fn(u32) -> u32, divider: u32, true_monkey: usize, false_monkey: usize) -> Monkey {
+    fn new(id: usize, items: Vec<u64>, operation: Box<dyn Fn(u64) -> u64>, divider: u64, true_monkey: usize, false_monkey: usize) -> Monkey {
         Monkey {
             id,
             items,
@@ -22,7 +26,7 @@ impl Monkey {
         }
     }
 
-    fn inspect(&mut self) -> (usize, u32) {
+    fn inspect(&mut self) -> (usize, u64) {
         self.inspect_count += 1;
         let item = self.items.remove(0);
         //println!("  Monkey inspects an item with a worry level of {}.", item);
@@ -44,21 +48,20 @@ impl Monkey {
 }
 
 fn main() {
-    /*let input = fs::read_to_string("input.txt").unwrap();
+    let input = fs::read_to_string("input.txt").unwrap();
     println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));*/
-
-    let mut monkeys = [
-        Monkey::new(0, vec![79, 98], |x| x * 19, 23, 2, 3),
-        Monkey::new(1, vec![54, 65, 75, 74], |x| x + 6, 19, 2, 0),
-        Monkey::new(2, vec![79, 60, 97], |x| x * x, 13, 1, 3),
-        Monkey::new(3, vec![74], |x| x + 3, 17, 0, 1)
-    ];
-
-    println!("Part 1: {}", part1(&mut monkeys));
+    println!("Part 2: {}", part2(&input));
 }
 
-fn part1(monkeys: &mut [Monkey]) -> u32 {
+fn part1(input: &str) -> u32 {
+    part1_parsed(&mut parse_input(input))
+}
+
+fn part2(input: &str) -> u32 {
+    0
+}
+
+fn part1_parsed(monkeys: &mut [Monkey]) -> u32 {
     let rounds = 20;
 
     for round in 0..rounds {
@@ -86,3 +89,69 @@ fn part1(monkeys: &mut [Monkey]) -> u32 {
     inspect_counts.sort();
     inspect_counts[inspect_counts.len() - 1] * inspect_counts[inspect_counts.len() - 2]
 }
+
+fn parse_input(input: &str) -> Vec<Monkey> {
+    let lines = input.lines().collect::<Vec<_>>();
+    let mut monkeys = Vec::new();
+
+    let id_re = Regex::new(r"Monkey (\d+):").unwrap();
+
+
+    for i in (0..lines.len()).step_by(7) {
+        let id = re_capture_1::<usize>(&id_re, lines[i]);
+        let items = &lines[i + 1][18..].split(", ")
+            .map(|x| x.parse::<u64>().unwrap())
+            .collect::<Vec<_>>();
+        let operation_str = &lines[i + 2][23..];
+        let operation: Box<dyn Fn(u64) -> u64> = if operation_str.chars().next().unwrap() == '+' {
+            let other = operation_str[2..].parse::<u64>().unwrap();
+            Box::new(move |x| x + other)
+        } else if operation_str == "* old" {
+            Box::new(|x| x * x)
+        } else {
+            let other = operation_str[2..].parse::<u64>().unwrap();
+            Box::new(move |x| x * other)
+        };
+        let divider = re_capture_1::<u64>(&Regex::new(r"Test: divisible by (\d+)").unwrap(), lines[i + 3]);
+        let true_monkey = re_capture_1::<usize>(&Regex::new(r"If true: throw to monkey (\d+)").unwrap(), lines[i + 4]);
+        let false_monkey = re_capture_1::<usize>(&Regex::new(r"If false: throw to monkey (\d+)").unwrap(), lines[i + 5]);
+
+        monkeys.push(Monkey::new(id, items.to_vec(), operation, divider, true_monkey, false_monkey));
+    }
+
+    monkeys
+}
+
+fn re_capture_1<T: FromStr>(re: &Regex, line: &str) -> T where <T as FromStr>::Err: Debug {
+    re.captures(line).unwrap().get(1).unwrap().as_str().parse::<T>().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use super::*;
+
+    fn parsed_test_input() -> [Monkey; 4] {
+        [
+            Monkey::new(0, vec![79, 98], Box::from(|x| x * 19), 23, 2, 3),
+            Monkey::new(1, vec![54, 65, 75, 74], Box::from(|x| x + 6), 19, 2, 0),
+            Monkey::new(2, vec![79, 60, 97], Box::from(|x| x * x), 13, 1, 3),
+            Monkey::new(3, vec![74], Box::from(|x| x + 3), 17, 0, 1)
+        ]
+    }
+
+    #[test]
+    fn test_part1_no_parsing() {
+        assert_eq!(part1_parsed(&mut parsed_test_input()), 10605);
+    }
+
+    #[test]
+    fn test_part1() {
+        let test_input = &fs::read_to_string("src/bin/day-11/test_input.txt").unwrap();
+        assert_eq!(part1_parsed(&mut parse_input(test_input)), 10605);
+
+        let input = &fs::read_to_string("src/bin/day-11/input.txt").unwrap();
+        assert_eq!(part1_parsed(&mut parse_input(input)), 88208);
+    }
+}
+
